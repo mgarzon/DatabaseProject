b1e97c2768f2f76398933c231be53a1cbcf18e16
Miguel Garzon <mgarzon@uottawa.ca>
2017-10-14 23:18:05 -0400

    ProductCatalog using SQLite

---
+++ DatabaseProject/app/src/main/java/com/seg2105/database/MainActivity.java
@@ -0,0 +1,74 @@
+package com.seg2105.database;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+public class MainActivity extends AppCompatActivity {
+
+    TextView idView;
+    EditText productBox;
+    EditText skuBox;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        idView = (TextView) findViewById(R.id.productID);
+        productBox = (EditText) findViewById(R.id.productName);
+        skuBox = (EditText) findViewById(R.id.productSku);
+    }
+
+
+
+    public void newProduct (View view) {
+
+        int sku = Integer.parseInt(skuBox.getText().toString());
+
+        Product product = new Product(productBox.getText().toString(), sku);
+
+        // TODO: add to database
+
+        productBox.setText("");
+
+        skuBox.setText("");
+    }
+
+
+    public void lookupProduct (View view) {
+
+        // TODO: get from Database
+        Product product = null;
+
+        if (product != null) {
+            idView.setText(String.valueOf(product.getID()));
+            skuBox.setText(String.valueOf(product.getSku()));
+        } else {
+            idView.setText("No Match Found");
+        }
+    }
+
+
+    public void removeProduct (View view) {
+
+        // TODO: remove from database
+        boolean result = false;
+
+        if (result) {
+            idView.setText("Record Deleted");
+            productBox.setText("");
+            skuBox.setText("");
+        }
+        else
+            idView.setText("No Match Found");
+    }
+
+
+    public void about(View view) {
+        Intent aboutIntent = new Intent(this, About.class);
+        startActivity(aboutIntent);
+    }
+}
