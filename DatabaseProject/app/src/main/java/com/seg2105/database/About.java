146fe3477f60b2f9293af9d0db4df855cfa38adb
alikhanafer7 <ali.h.khanafer@gmail.com>
2020-10-23 16:18:32 -0400

    fixed all issues

--- DatabaseProject/app/src/main/java/com/seg2105/database/About.java
+++ DatabaseProject/app/src/main/java/com/seg2105/database/About.java
@@ -1,6 +1,6 @@
 package com.seg2105.database;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class About extends AppCompatActivity {
